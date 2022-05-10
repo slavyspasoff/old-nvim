697a2cddcf5ce8c55443fd6ae92142203c712681
Slavy Spasoff <slavyspasoff@gmail.com>
2022-05-10 19:11:57 +0200

    add autocmd to show errors on hover instead on call

--- lua/modules/lsp/init.lua
+++ lua/modules/lsp/init.lua
@@ -5,3 +5,6 @@ end
 
 require("modules.lsp.lsp-installer")
 require("modules.lsp.handlers").setup()
+
+-- vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]   -- for both normal and insert mode
+vim.cmd [[autocmd! CursorHold * lua vim.diagnostic.open_float(nil, {focus=false})]]
